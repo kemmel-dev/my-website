daa2e531e96846cdfd579e23988f1ae5be45df10
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-04 18:33:16 +0100

    Updates

--- _app/immutable/chunks/paths.DEcEA_mw.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_oda5z0)==null?void 0:s.base)??"/my-website";var a;const t=((a=globalThis.__sveltekit_oda5z0)==null?void 0:a.assets)??e;export{t as a,e as b};
