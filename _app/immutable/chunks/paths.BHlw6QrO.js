77396ecc8660fbe7d6cb57a114002de18011c20d
Kamiel de Visser <kamieldevisser@gmail.com>
2024-03-04 18:38:34 +0100

    Updates

---
+++ _app/immutable/chunks/paths.BHlw6QrO.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_t9lb27)==null?void 0:s.base)??"/my-website";var t;const a=((t=globalThis.__sveltekit_t9lb27)==null?void 0:t.assets)??e;export{a,e as b};
--- _app/immutable/chunks/paths.DyeWOrZK.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_ga08vo)==null?void 0:s.base)??"/my-website";var a;const t=((a=globalThis.__sveltekit_ga08vo)==null?void 0:a.assets)??e;export{t as a,e as b};
